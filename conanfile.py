dd68b73122756a7a0d4cd61e2152a78396e5b50d
Ulrich Eck <ueck@net-labs.de>
2023-12-09 16:58:01 +0100

    updated windows variant

--- conanfile.py
+++ conanfile.py
@@ -44,6 +44,14 @@ class OrbbecSDKConan(ConanFile):
             sub_version = "1.8.3"
             dl_url = f"https://dl.orbbec3d.com/dist/orbbecsdk/{sub_version}/OrbbecSDK_{sub_version}_Windows.zip"
             folder_name = "OrbbecSDK_1.8.3_Windows"
+            if self.settings.arch == "x86":
+                sub_archive_match = "OrbbecSDK_C_C++_v*_win_x86_release.zip"
+            elif self.settings.arch == "x86_64":
+                sub_archive_match = "OrbbecSDK_C_C++_v*_win_x64_release.zip"
+            else:
+                    raise EnvironmentError("Unsupported OS")
+
+
         if dl_url is None:
             raise EnvironmentError("Unsupported OS")
         get(self, dl_url, destination=self.source_folder)
@@ -60,24 +68,31 @@ class OrbbecSDKConan(ConanFile):
     def package(self):
 
         sdk_root = os.path.join(self.build_folder, "dist", "SDK")
+        copy(self, "*.h*", 
+            os.path.join(sdk_root, "include"), 
+            os.path.join(self.package_folder, "include"))
+
+        copy(self, "OrbbecSDKConfig_*.xml", 
+            os.path.join(sdk_root, "config"), 
+            os.path.join(self.package_folder, "config"))
+
         if self.settings.os == "Macos":
-            copy(self, "*.h*", 
-                os.path.join(sdk_root, "include"), 
-                os.path.join(self.package_folder, "include"))
             copy(self, "*.dylib", 
                 os.path.join(sdk_root, "lib"), 
                 os.path.join(self.package_folder, "lib"))
             copy(self, "*.a", 
                 os.path.join(sdk_root, "lib"), 
                 os.path.join(self.package_folder, "lib"))
-            copy(self, "OrbbecSDKConfig_*.xml", 
-                os.path.join(sdk_root, "config"), 
-                os.path.join(self.package_folder, "config"))
 
         elif self.settings.os == "Linux":
             raise NotImplementedError()
         elif self.settings.os == "Windows":
-            raise NotImplementedError()
+            copy(self, "*.dll", 
+                os.path.join(sdk_root, "lib"), 
+                os.path.join(self.package_folder, "bin"))
+            copy(self, "*.lib", 
+                os.path.join(sdk_root, "lib"), 
+                os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
         self.cpp_info.libs = ["OrbbecSDK"]
