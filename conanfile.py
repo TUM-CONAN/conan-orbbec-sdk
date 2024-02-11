c7f4787561a5461d425a2c52aea5e720c8d86907
ulrich eck <ulrich.eck@tum.de>
2024-02-11 13:28:54 +0100

    moved to github releases, upgraded to 1.9.4

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ from io import StringIO
 
 class OrbbecSDKConan(ConanFile):
     name = "orbbec-sdk"
-    version = "1.8"
+    version = "1.9.4"
 
     description = "Orbbec Camera SDK"
     url = "https://github.com/TUM-CAMP-NARVIS/conan-orbbec-sdk.git"
@@ -27,89 +27,53 @@ class OrbbecSDKConan(ConanFile):
     def layout(self):
         basic_layout(self)
 
-    def _package_description(self):
-        dl_url = None
-        folder_name = None
-        sub_version = None
-        sub_archive_match = None
-        if self.settings.os == "Macos":
-            sub_version = "1.8.1"
-            dl_url = f"https://dl.orbbec3d.com/dist/orbbecsdk/{sub_version}/OrbbecSDK_{sub_version}_macOS.zip"
-            folder_name = "OrbbecSDK_1.8.1_macOS"
-            sub_archive_match = "OrbbecSDK_C_C++_v*_macos_arm64_release.zip"
-        elif self.settings.os == "Linux":
-            sub_version = "1.8.3"
-            dl_url = f"https://dl.orbbec3d.com/dist/orbbecsdk/{sub_version}/OrbbecSDK_{sub_version}_Linux.zip"
-            folder_name = "OrbbecSDK_1.8.3_Linux"
-            if self.settings.arch == "x86_64":
-                sub_archive_match = "OrbbecSDK_C_C++_v*_linux_x64_release.zip"
-            elif self.settings.arch == "armv8":
-                sub_archive_match = "OrbbecSDK_C_C++_v*_linux_arm64_release.zip"
-            else:
-                raise EnvironmentError("Unsupported OS")
-        elif self.settings.os == "Windows":
-            sub_version = "1.8.3"
-            dl_url = f"https://dl.orbbec3d.com/dist/orbbecsdk/{sub_version}/OrbbecSDK_{sub_version}_Windows.zip"
-            folder_name = "OrbbecSDK_1.8.3_Windows"
-            if self.settings.arch == "x86":
-                sub_archive_match = "OrbbecSDK_C_C++_v*_win_x86_release.zip"
-            elif self.settings.arch == "x86_64":
-                sub_archive_match = "OrbbecSDK_C_C++_v*_win_x64_release.zip"
-            else:
-                raise EnvironmentError("Unsupported OS")
-
-
-        if dl_url is None:
-            raise EnvironmentError("Unsupported OS")
-        return (dl_url, folder_name, sub_version, sub_archive_match)        
-
     def build(self):
-        (dl_url, folder_name, sub_version, sub_archive_match) = self._package_description()
-        get(self, dl_url, destination=self.source_folder)
-
-        files = glob.glob(os.path.join(self.source_folder, folder_name, sub_archive_match))
-        if len(files) != 1:
-            raise ValueError("Missing Archive.")
-
-        # this does now work correctly with symlinks on osx and maybe linux
-        if self.settings.os == "Macos" or self.settings.os == "Linux":
-            # python zipfile does not correctly extract the symlinks for the dylib
-            self.run("unzip {} -d {}".format(files[0], os.path.join(self.source_folder, "OrbbecSDK")))
-        else:
-            unzip(self, files[0], os.path.join(self.source_folder, "OrbbecSDK"))
+        get(self, f"https://github.com/orbbec/OrbbecSDK/archive/refs/tags/v{self.version}.tar.gz", destination=self.source_folder)
 
 
     def package(self):
-        (dl_url, folder_name, sub_version, sub_archive_match) = self._package_description()
-
-        sdk_root = os.path.join(self.source_folder, "OrbbecSDK", f"OrbbecSDK_v{sub_version}", "SDK")
+        sdk_root = os.path.join(self.source_folder, f"OrbbecSDK-{self.version}")
         copy(self, "*.h*", 
             os.path.join(sdk_root, "include"), 
             os.path.join(self.package_folder, "include"))
 
         copy(self, "OrbbecSDKConfig_*.xml", 
-            os.path.join(sdk_root, "config"), 
+            os.path.join(sdk_root, "misc", "config"), 
             os.path.join(self.package_folder, "config"))
 
         if self.settings.os == "Macos":
             copy(self, "*.dylib", 
-                os.path.join(sdk_root, "lib"), 
+                os.path.join(sdk_root, "lib", "MacOS"), 
                 os.path.join(self.package_folder, "lib"))
             copy(self, "*.a", 
-                os.path.join(sdk_root, "lib"), 
+                os.path.join(sdk_root, "lib", "MacOS"), 
                 os.path.join(self.package_folder, "lib"))
 
         elif self.settings.os == "Linux":
-            copy(self, "*.so*", 
-                os.path.join(sdk_root, "lib"), 
-                os.path.join(self.package_folder, "lib"))
+            if self.settings.arch == "x86_64":
+                copy(self, "*.so*", 
+                    os.path.join(sdk_root, "lib", "linux_x64"), 
+                    os.path.join(self.package_folder, "lib"))
+            elif self.settings.arch == "armv8":
+                copy(self, "*.so*", 
+                    os.path.join(sdk_root, "lib", "arm64"), 
+                    os.path.join(self.package_folder, "lib"))
+
         elif self.settings.os == "Windows":
-            copy(self, "*.dll", 
-                os.path.join(sdk_root, "lib"), 
-                os.path.join(self.package_folder, "bin"))
-            copy(self, "*.lib", 
-                os.path.join(sdk_root, "lib"), 
-                os.path.join(self.package_folder, "lib"))
+            if self.settings.arch == "x86_64":
+                copy(self, "*.dll", 
+                    os.path.join(sdk_root, "lib", "win_x64"), 
+                    os.path.join(self.package_folder, "bin"))
+                copy(self, "*.lib", 
+                    os.path.join(sdk_root, "lib", "win_x64"), 
+                    os.path.join(self.package_folder, "lib"))
+            elif self.settings.arch == "x86":
+                copy(self, "*.dll", 
+                    os.path.join(sdk_root, "lib", "win_x32"), 
+                    os.path.join(self.package_folder, "bin"))
+                copy(self, "*.lib", 
+                    os.path.join(sdk_root, "lib", "win_x32"), 
+                    os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
         self.cpp_info.libs = ["OrbbecSDK"]
